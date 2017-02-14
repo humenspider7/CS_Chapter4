c2149b8ed4a25e69e20fe31b634f83167a8ee2a6
humenspider7 <iamyusuf6@aol.com>
2017-02-13 20:15:32 -0500

    Finished Multiplication Table

--- CS_Chapter4/MultiplicationTable/Program.cs
+++ CS_Chapter4/MultiplicationTable/Program.cs
@@ -7,9 +7,18 @@ using System.Threading.Tasks;
 namespace MultiplicationTable
 {
     class Program
-    {
+    {   //DONE
         static void Main(string[] args)
         {
+            for(int i = 1; i <= 10; i++)
+            {
+                for( int y = 1; y <= 10; y++)
+                {
+                    Console.Write(y * i + "\t");
+                }
+                Console.Write("\n\n");
+
+            }
         }
     }
 }
